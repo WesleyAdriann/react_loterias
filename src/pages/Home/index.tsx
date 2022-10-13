582354f7df9f1e6fe5f5f4859b00e794b1443f7b
Rafael Makaha <rafael.makaha@gmail.com>
2022-10-13 12:25:11 -0300

    Add styled main page
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,36 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
-
-export const Container = styled.div``
+import React, { FC } from "react";
+import { Container, InfoFooter, Logo, LotteryNumber, LotteryWrapper, MainTitle, ResultsContainer, SectionValue, SectionWrapper, Select, SideContainer, Text, Wrapper } from "./style";
+import logo from '../../assets/Logo.svg'
 
 const Home: FC = () => {
   return (
-    <Container>Hello World</Container>
+    <Container>
+      <SideContainer>
+        <Select/>
+        <Wrapper>
+          <Logo src={logo} />
+          <MainTitle>MegaSena</MainTitle>
+        </Wrapper>
+        <SectionWrapper>
+          <Text>
+            Concurso
+          </Text>
+          <SectionValue>
+            4531-07/04/2020
+          </SectionValue>
+        </SectionWrapper>
+      </SideContainer>
+      <ResultsContainer>
+        <LotteryWrapper>
+          {Array(6).fill(12).map((value: number, index: number) => (
+            <LotteryNumber key={index}>{value}</LotteryNumber>
+          ))
+          }
+        </LotteryWrapper>
+        <InfoFooter>Este sorteio é meramente ilustrativo e não possui nenhuma ligação com a CAIXA.</InfoFooter>
+      </ResultsContainer>
+    </Container>
   );
-}
- 
+};
+
 export default Home;
